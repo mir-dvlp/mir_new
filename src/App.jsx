550fec38e799b94dc2ed928cad459eb7580e18ca
Michael Ivanov <mikenway72@gmail.com>
2023-02-03 17:51:24 +0300

    update 1.0

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function App() {
             <Card type="icon_card hover" href="https://github.com/mir-dvlp" src="/s_github.webp" name="GitHub"/>
           </div>
           <div className='column'>
-            <Card type="item_card hover" href="https://langame.ru" src="/ic_langame.svg" name="LANGAME Design"/>
-            <Card type="item_card hover" href="https://my-study-projects.netlify.app/" src="/ic_code.svg" name="My Study Projects"/>
+            <Card type="item_card hover lg" href="https://langame.ru" src="/ic_langame.svg" name="LANGAME Design"/>
+            <Card type="item_card hover msp" href="https://my-study-projects.netlify.app/" src="/ic_code.svg" name="My Study Projects"/>
           </div>
           <div class="promote">Хотите также?<a href="https://t.me/mir_dsgn" aria-label="Write Me">Напишите мне</a></div>
       </div>
